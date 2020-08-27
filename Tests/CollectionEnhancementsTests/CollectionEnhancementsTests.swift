dd7334823626096ab712e1d8b9808946e14e8a63
Sam Lazarus <tyu.sam@gmail.com>
2020-08-27 13:02:23 -0400

    Removed broken template test

--- Tests/CollectionEnhancementsTests/CollectionEnhancementsTests.swift
+++ Tests/CollectionEnhancementsTests/CollectionEnhancementsTests.swift
@@ -2,14 +2,5 @@ import XCTest
 @testable import CollectionEnhancements
 
 final class CollectionEnhancementsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CollectionEnhancements().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    // TODO: Add tests and re-add allTests
 }
