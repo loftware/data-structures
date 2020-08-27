7ea2fc5cee51372f207f9263f37f3d32d5b5eb2f
Sam Lazarus <tyu.sam@gmail.com>
2020-08-27 16:39:02 -0400

    Now only reexports other libraries, added export checks

--- Package.swift
+++ Package.swift
@@ -23,14 +23,22 @@ let package = Package(
         .package(
             name: "Either",
             url: "git@github.com:loftware/either-type.git",
-            .branch("master"))
+            .branch("master")),
+        .package(
+            name: "ConcatenatedCollection",
+            url: "git@github.com:loftware/concatenated-collection.git",
+            .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CollectionEnhancements",
-            dependencies: ["Either"]),
+            dependencies: [
+                "Either",
+                "ConcatenatedCollection"
+            ]
+        ),
         .testTarget(
             name: "CollectionEnhancementsTests",
             dependencies: ["CollectionEnhancements"]),
---
+++ Sources/CollectionEnhancements/CollectionEnhancements.swift
@@ -0,0 +1,2 @@
+@_exported import Either
+@_exported import ConcatenatedCollection
